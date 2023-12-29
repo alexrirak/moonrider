cd595f8da7a47b6aac77acd6e04e566e54bf124e
Alex Rirak <alexrirak@gmail.com>
2023-12-28 20:56:27 -0500

    allow initial score submit to see board

--- src/state/index.js
+++ src/state/index.js
@@ -457,12 +457,17 @@ AFRAME.registerState({
      * Insert new score into leaderboard locally.
      */
     leaderboardscoreadded: (state, payload) => {
-      // Insert.
-      for (let i = 0; i < state.leaderboard.length; i++) {
-        if (payload.scoreData.score >= state.leaderboard[i].score ||
-          i >= state.leaderboard.length - 1) {
-          state.leaderboard.splice(i, 0, payload.scoreData);
-          break;
+      // If leaderboard is empty, add the score directly
+      if (state.leaderboard.length === 0) {
+        state.leaderboard.push(payload.scoreData);
+      } else {
+        // Insert.
+        for (let i = 0; i < state.leaderboard.length; i++) {
+          if (payload.scoreData.score >= state.leaderboard[i].score ||
+              i >= state.leaderboard.length - 1) {
+            state.leaderboard.splice(i, 0, payload.scoreData);
+            break;
+          }
         }
       }
 
